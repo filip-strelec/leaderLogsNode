57d585dabf4ce1cf1da4c87bed6e4af2f912671f
Filip Strelec <filip.strelec@gmail.com>
2022-10-08 04:42:36 +0200

    Small fixes

--- getLeaderlogs.sh
+++ getLeaderlogs.sh
@@ -4,7 +4,7 @@ echo "Reading env..."  >&2
 #source env.cfg
 echo "Pool ID $pool_id" >&2
 
-echo "Deleting old stakeSnapshot.json" >&2
+echo "Deleting old stakeSnapshot$pool_ticker.json" >&2
 rm ./results/stakeSnapshot$pool_ticker.json
 echo "Running cardano-cli query stake-snapshot..." >&2
 cardano-cli query stake-snapshot --stake-pool-id $pool_id --mainnet >> ./results/stakeSnapshot$pool_ticker.json
@@ -14,7 +14,7 @@ echo "POOL_STAKE  $POOL_STAKE"
 ACTIVE_STAKE=$(jq .activeStakeMark ./results/stakeSnapshot$pool_ticker.json)
 echo "ACTIVE STAKE  $ACTIVE_STAKE"
 
-echo "Deleting old leaderlogs.json" >&2
+echo "Deleting old leaderlogs.json$pool_ticker" >&2
 rm ./results/leaderlogs$pool_ticker.json
 echo "Running CNCLI leaderlog for $pool_id..." >&2
 #Taskset is used to assign a task to 0-5 cores (delete taskset -c 0,1,2,3,4,5 if you want to use all cores )
