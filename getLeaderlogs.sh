176b6b45b8c28ce75228af9476660e5ae2a9734a
Filip Strelec <filip.strelec@gmail.com>
2022-10-08 03:56:05 +0200

    Add canvas

--- getLeaderlogs.sh
+++ getLeaderlogs.sh
@@ -7,7 +7,7 @@ echo "Pool ID $pool_id" >&2
 echo "Deleting old stakeSnapshot.json" >&2
 rm ./results/stakeSnapshot.json
 echo "Running cardano-cli query stake-snapshot..." >&2
-cardano-cli query stake-snapshot --stake-pool-id $pool_id --mainnet >> ./results/stakeSnapshot.json
+cardano-cli query stake-snapshot --stake-pool-id $pool_id --mainnet >> ./results/stakeSnapshot$pool_ticker.json
 
 POOL_STAKE=$(jq .poolStakeMark ./results/stakeSnapshot.json)
 echo "POOL_STAKE  $POOL_STAKE"
@@ -18,10 +18,10 @@ echo "Deleting old leaderlogs.json" >&2
 rm ./results/leaderlogs.json
 echo "Running CNCLI leaderlog for $pool_id..." >&2
 #Taskset is used to assign a task to 0-5 cores (delete taskset -c 0,1,2,3,4,5 if you want to use all cores )
-taskset -c 0,1,2,3,4,5 cncli leaderlog --pool-id $pool_id --pool-vrf-skey $vrf_key_location --byron-genesis $byron_genesis_location  --shelley-genesis $shelley_genesis_location  --active-stake $ACTIVE_STAKE --pool-stake $POOL_STAKE --ledger-set $search_type >> ./results/leaderlogs.json
+taskset -c 0,1,2,3,4,5 cncli leaderlog --pool-id $pool_id --pool-vrf-skey $vrf_key_location --byron-genesis $byron_genesis_location  --shelley-genesis $shelley_genesis_location  --active-stake $ACTIVE_STAKE --pool-stake $POOL_STAKE --ledger-set $search_type >> ./results/leaderlogs$pool_ticker.json
 echo "CNCLI leaderlog FINISHED" >&2
 echo "Starting image generation" >&2
 sleep 1
-curl localhost:8080/trigger?pool=venus
+curl localhost:8080/trigger?pool=$pool_ticker
 sleep 5
 echo "Bash script finished"
--- node.js
+++ node.js
@@ -5,8 +5,10 @@ dotenv.config();
 var app = express();
 //const { exec } = require('child_process');
 const schedule = require('node-schedule');
-
-
+const { createCanvas, loadImage } = require("canvas");
+const fs = require("fs");
+const client = require('https');
+app.use(express.static('./pngOutput'));
 let nodeStartedDate = new Date();
 const millisecondsInFiveDays = 4.32 * Math.pow(10, 8)
 
@@ -22,6 +24,111 @@ let testSchedule = (executionDate) => {
         testSchedule(executionDate);
         initializeScript();
     });
+}
+
+
+async function downloadImage(url, filepath) {
+    return new Promise((resolve, reject) => {
+        client.get(url, (res) => {
+            if (res.statusCode === 200) {
+                res.pipe(fs.createWriteStream(filepath))
+                    .on('error', reject)
+                    .once('close', () => resolve(filepath));
+            } else {
+                // Consume response data to free up memory
+                res.resume();
+                reject(new Error(`Request Failed With a Status Code: ${res.statusCode}`));
+
+            }
+        });
+    });
+}
+
+
+
+
+
+const getJsonFromFile = (poolTicker) => {
+    let jsonified = "";
+    try {
+        const rawdata = fs.readFileSync(`./results/leaderlogs${poolTicker}.json`);
+        jsonified = JSON.parse(rawdata);
+    }
+
+    catch (e) {
+        console.log("invalid json", e);
+    }
+    return jsonified;
+}
+
+const canvasDrawAndExport = async (poolTicker) => {
+    const poolID = process.env.pool_id;
+    const logoURL = `https://cdn.adapools.org/pool_logo/${poolID}.png`;
+    await downloadImage(logoURL, `./pngOutput/${poolTicker}Ticker.png`);
+    const stakeSnapshotJson = getJsonFromFile(poolTicker);
+    const epoch = stakeSnapshotJson.epoch;
+    const epochSlots = stakeSnapshotJson.epochSlots;
+    const assignedSlots = stakeSnapshotJson.assignedSlots;
+    console.log("Pool Ticker:" + poolTicker);
+    console.log("EPOCH:" + epoch);
+    console.log("Nr. of slots assigned:" + epochSlots);
+
+    const width = 1600;
+    const height = 900;
+
+    const canvas = createCanvas(width, height);
+    const context = canvas.getContext("2d");
+    context.fillStyle = "#764abc";
+    context.fillRect(0, 0, width, height);
+
+    context.font = "bold 52pt 'PT Sans'";
+    context.textAlign = "center";
+    context.fillStyle = "#fff";
+    context.fillText(`Epoch:${epoch}`, width / 2, 100);
+    context.font = "bold 54pt 'PT Sans'";
+    context.fillText(`Pool:${poolTicker}`, width / 2, 170);
+    context.font = "bold 22pt 'PT Sans'";
+    context.textAlign = "start";
+    context.fillText(`Nr. of slots:${epochSlots}`, 1200, 119);
+    context.textAlign = "center";
+    context.fillText(`Slots in Epoch`, width / 2, 230);
+
+
+    // assignedSlots.forEach((element)=>{
+    //     console.log(element,"BOK")
+    // })
+
+    loadImage(`./resources/block.png`).then((image) => {
+        context.textAlign = "start";
+
+        let rowCounter = 1;
+        let columnCounter = 0;
+        assignedSlots.forEach((element) => {
+            (parseInt(element.no - 1) % 3 === 0) && rowCounter++;
+            (parseInt(element.no - 1) % 3 === 0) && (columnCounter = 0);
+            context.fillText(`${element.no}: Slot in Epoch:${element.slotInEpoch}`, 130 + (parseInt(columnCounter) * 460), 270 + parseInt(rowCounter) * 50);
+            columnCounter++;
+        })
+
+
+    });
+
+
+    const PoolimagePosition = {
+        w: 88,
+        h: 88,
+        x: 75,
+        y: 75,
+    };
+    loadImage(`./pngOutput/${poolTicker}Ticker.png`).then((image) => {
+        const { w, h, x, y } = PoolimagePosition;
+        context.drawImage(image, x, y, w, h);
+        const buffer = canvas.toBuffer("image/png");
+        fs.writeFileSync(`./pngOutput/image${poolTicker}.png`, buffer);
+    });
+
+
+
 }
 
 
@@ -47,21 +154,20 @@ const initializeScript = () => {
 
 }
 
+//INIT!!!
 testSchedule(executionDate);
 initializeScript();
 
-//define the route for "/"
+
+
 app.get("/", function (request, response) {
-    //show this file when the "/" is requested
     response.sendFile(__dirname + "/index.html");
 });
 
 app.get("/trigger", function (request, response) {
-    //show this file when the "/" is requested
-const poolTicker = request.query.pool;
-
-console.log(poolTicker);
-response.end("triggerTriggered");
+    const poolTicker = request.query.pool;
+    canvasDrawAndExport(poolTicker.toUpperCase());
+    response.end("Triggered Creating images for: " + poolTicker);
 });
 
 console.log("starting the web server at localhost:8080");
