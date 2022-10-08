1f9d9eb1f7746cb10e192f356655054fd5d8c0a7
Filip Strelec <filip.strelec@gmail.com>
2022-10-08 04:15:20 +0200

    Fixes

--- getLeaderlogs.sh
+++ getLeaderlogs.sh
@@ -5,7 +5,7 @@ echo "Reading env..."  >&2
 echo "Pool ID $pool_id" >&2
 
 echo "Deleting old stakeSnapshot.json" >&2
-rm ./stakeSnapshot$pool_ticker.json
+rm ./results/stakeSnapshot$pool_ticker.json
 echo "Running cardano-cli query stake-snapshot..." >&2
 cardano-cli query stake-snapshot --stake-pool-id $pool_id --mainnet >> ./results/stakeSnapshot$pool_ticker.json
 
@@ -15,7 +15,7 @@ ACTIVE_STAKE=$(jq .activeStakeMark ./results/stakeSnapshot$pool_ticker.json)
 echo "ACTIVE STAKE  $ACTIVE_STAKE"
 
 echo "Deleting old leaderlogs.json" >&2
-rm ./leaderlogs$pool_ticker.json
+rm ./results/leaderlogs$pool_ticker.json
 echo "Running CNCLI leaderlog for $pool_id..." >&2
 #Taskset is used to assign a task to 0-5 cores (delete taskset -c 0,1,2,3,4,5 if you want to use all cores )
 taskset -c 0,1,2,3,4,5 cncli leaderlog --pool-id $pool_id --pool-vrf-skey $vrf_key_location --byron-genesis $byron_genesis_location  --shelley-genesis $shelley_genesis_location  --active-stake $ACTIVE_STAKE --pool-stake $POOL_STAKE --ledger-set $search_type >> ./results/leaderlogs$pool_ticker.json
